2116634245cbd63108672d0f5b541cae93fdc5df
Zakir Hossain <54552612+programmerzakir@users.noreply.github.com>
2020-02-25 07:57:26 +0600

    Add files via upload

---
+++ build/precache-manifest.3a4c251eef4836e0e5aba8e823fbffcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14cbe11776b33d1757ba989570bfd9ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cf24bc040727997cde9",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d57c275cca8cfdfee45a",
+    "url": "/static/js/2.4c0f4877.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.4c0f4877.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cf24bc040727997cde9",
+    "url": "/static/js/main.3ee0900d.chunk.js"
+  },
+  {
+    "revision": "b777e1e5607df564d26b",
+    "url": "/static/js/runtime-main.aaf88d20.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
